4f435e9c7185ba1f68070922df9a0d1dc9580cc6
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-10-21 11:00:46 -0400

    added PickerView and delegates

--- SocietyLibrary/Controllers/BestsellersVC.swift
+++ SocietyLibrary/Controllers/BestsellersVC.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class BestsellersVC: UIViewController {
 
+    var bestBooks = [Category]()
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
-    
+        setUpDelegates()
+    }
+    private func setUpDelegates(){
+        bookPicker.delegate = self
+        bookPicker.dataSource = self
+        
     }
     private func setUpView(){
         self.navigationItem.title = "NYT Bestsellers"
         view.backgroundColor = .white
     }
+    lazy var bookPicker: UIPickerView = {
+      let bookPicker = UIPickerView()
+        return bookPicker
+    }()
 
+}
+extension BestsellersVC: UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return bestBooks.count
 
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let book =  bestBooks[row]
+        return book
+    }
+    
+    
 }
 
