d843c17905ac704ec8428be6a16da1498259fb42
Tia Lendor <TiaLendon@pursuit.org>
2019-10-22 16:48:01 -0400

    sumthin final, action sheet and finished persistence

--- SocietyLibrary/CustomCells/FavoriteBookCell.swift
+++ SocietyLibrary/CustomCells/FavoriteBookCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class FavoriteBookCell: UICollectionViewCell {
     
+    weak var delegate: BookCellDelegate?
+    
     lazy var faveBookImage: UIImageView = {
         let favedImage = UIImageView()
         return favedImage
@@ -17,6 +19,7 @@ class FavoriteBookCell: UICollectionViewCell {
     
     lazy var bookSummary: UITextView = {
         var summary = UITextView()
+        summary.backgroundColor = .orange
         return summary
     }()
     
@@ -25,6 +28,43 @@ class FavoriteBookCell: UICollectionViewCell {
         return topWeeks
     }()
     
+    lazy var actionButton: UIButton = {
+        var button = UIButton()
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(showActionS(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+  
+        
+    @objc func showActionS (_ sender: UIButton) {
+        delegate?.showActionSheet(tag: sender.tag)
+        }
+//                try? book.permanager.delete(element: self.photoJournal, atIndex: tag)
+//                self.loadJournal()
+//            }
+//            let editAction = UIAlertAction(title: "Edit", style: .default) { (action) in
+//                let alert = UIAlertController(title: "", message: "I DO NOT KNOW HOW TO EDIT", preferredStyle: .alert)
+//                let action = UIAlertAction(title: "OK", style: .destructive, handler: nil)
+//                alert.addAction(action)
+//                self.present(alert, animated: true)
+//            }
+//            let shareAction = UIAlertAction(title: "Share", style: .default) { (action) in
+//                let alert = UIAlertController(title: "", message: "I DO NOT KNOW HOW TO SHARE", preferredStyle: .alert)
+//                let action = UIAlertAction(title: "OK", style: .destructive, handler: nil)
+//                alert.addAction(action)
+//                self.present(alert, animated: true)
+//            }
+//            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+//            }
+//            alert.addAction(destroyAction)
+//            alert.addAction(editAction)
+//            alert.addAction(shareAction)
+//            alert.addAction(cancelAction)
+//            self.present(alert, animated: true) {
+//            }
+//        }
+
     
     
 //    lazy var externalURL:
@@ -45,6 +85,8 @@ class FavoriteBookCell: UICollectionViewCell {
         addSubview(faveBookImage)
         addSubview(weeksAtTop)
         addSubview(bookSummary)
+        addSubview(actionButton)
+        
       
     }
     
@@ -52,13 +94,26 @@ class FavoriteBookCell: UICollectionViewCell {
     func constrain() {
         
         faveBookImage.translatesAutoresizingMaskIntoConstraints = false
-        [faveBookImage.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 50), faveBookImage.bottomAnchor.constraint(equalTo: weeksAtTop.topAnchor, constant: -50), faveBookImage.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 50), faveBookImage.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -50)].forEach{ $0.isActive = true }
+        [faveBookImage.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 50),
+         faveBookImage.heightAnchor.constraint(equalToConstant: 100),
+         faveBookImage.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+         faveBookImage.widthAnchor.constraint(equalToConstant: 100)].forEach{ $0.isActive = true }
         
        weeksAtTop.translatesAutoresizingMaskIntoConstraints = false
-       [weeksAtTop.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor), weeksAtTop.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor)].forEach{ $0.isActive = true }
+       [weeksAtTop.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+        weeksAtTop.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor)].forEach{ $0.isActive = true }
         
         bookSummary.translatesAutoresizingMaskIntoConstraints = false
-        [bookSummary.topAnchor.constraint(equalTo: weeksAtTop.bottomAnchor), bookSummary.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor)].forEach{ $0.isActive = true }
+        [bookSummary.topAnchor.constraint(equalTo: weeksAtTop.bottomAnchor),
+         bookSummary.heightAnchor.constraint(equalToConstant: 200),
+         bookSummary.widthAnchor.constraint(equalToConstant: 200),
+         bookSummary.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor)].forEach{ $0.isActive = true }
+        
+        actionButton.translatesAutoresizingMaskIntoConstraints = false
+        [actionButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+         actionButton.leadingAnchor.constraint(equalTo: faveBookImage.trailingAnchor, constant: 20),
+        actionButton.widthAnchor.constraint(equalToConstant: 50),
+            actionButton.heightAnchor.constraint(equalToConstant: 50)].forEach{ $0.isActive = true }
     }
     
     override init(frame: CGRect) {
